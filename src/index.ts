f91205b2656d52fae530b5d2dbd6e03836d925fc
Mario Arnautou <mario.arnautou@gmail.com>
2019-10-02 08:30:59 +0200

    fix(api): export correctly pagination mode

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-unresolved */
 import Model from './base-model';
+import PaginationMode from './paginate-mode';
 
 export * from './update-operators';
 export * from './operators';
+export { PaginationMode };
 export { IFilterConditions } from './build-keys';
-export * from './paginate-mode';
 
 export default Model;
