f740c89eaf4dae406ec979e8ecc294e2c23b584d
Mario Arnautou <mario.arnautou@gmail.com>
2019-10-02 07:37:44 +0200

    fix(api): export pagination mode enum

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import Model from './base-model';
 export * from './update-operators';
 export * from './operators';
 export { IFilterConditions } from './build-keys';
+export * from './paginate-mode';
 
 export default Model;
