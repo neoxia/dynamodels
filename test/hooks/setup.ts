5976a37f0a08c97027b9de508ac1d6b19023dff1
Mario Arnautou <mario.arnautou@gmail.com>
2020-03-29 16:52:37 +0200

    chore: bump dependencies

--- test/hooks/setup.ts
+++ test/hooks/setup.ts
@@ -1,13 +1,9 @@
 /* eslint-disable import/no-unresolved,no-unused-vars,no-console */
 import { createTables } from './create-tables';
-import spawnDynamoDB from './spawn-dynamodb';
 /* eslint-disable import/no-unresolved,no-unused-vars */
 
 export default async (): Promise<void> => {
   try {
-    const port: number = parseInt(process.env.LOCAL_DYNAMODB_PORT, 10) || 8000;
-    console.log(`\nStarting dynamoDB offline on port ${port}...`);
-    await spawnDynamoDB(port);
     await createTables();
   } catch (e) {
     console.error(e);
