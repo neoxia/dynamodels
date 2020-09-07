9e70b4ca96d0a3eafb300f5c477eab575acc45ab
Mario <mario.arnautou@gmail.com>
2020-09-07 17:54:39 +0200

    fix(pagination): error in type declaration

--- src/paginate.ts
+++ src/paginate.ts
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-unresolved,no-unused-vars */
-import { DocumentClient, Key } from 'aws-sdk/clients/dynamodb';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import PaginationMode from './paginate-mode';
+import { Key } from './base-model';
 /* eslint-enable import/no-unresolved,no-unused-vars */
 
 export interface IPaginationOptions {
   mode?: PaginationMode;
-  lastEvaluatedKey?: Key;
+  lastEvaluatedKey?: { [attribute: string]: Key };
   size: number;
 }
 
